f5ffdd81f4562cba463e026ea8622ff703f20a15
Asdoos <andi.riess@gmail.com>
2021-10-15 12:39:36 +0200

    Update env-init.sh

--- MQTT/env-init.sh
+++ MQTT/env-init.sh
@@ -1,2 +1,2 @@
-mkdir -p /etc/mosquitto/conf.d/mosquitto.conf
+mkdir -p /etc/mosquitto/conf.d/
 curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/files/mosquitto.conf > /etc/mosquitto/conf.d/mosquitto.conf
