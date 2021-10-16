428ceb730f65a3d8f3e63111b364b81baa4a205f
Asdoos <andi.riess@gmail.com>
2021-10-16 15:48:37 +0200

    wip

--- MQTT/env-init.sh
+++ MQTT/env-init.sh
@@ -1,11 +1,12 @@
 mkdir -p /etc/mosquitto/conf.d/
 curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/files/mosquitto.conf > /etc/mosquitto/conf.d/mosquitto.conf
 
-mkdir -p /scripts/fenster/
-curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/scripts/fenster/main.py > /scripts/fenster/main.py
-curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/scripts/fenster/requirements.txt > /scripts/fenster/requirements.txt
+mkdir -p /scripts/
+curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/scripts/fenster.py > /scripts/fenster.py
+curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/scripts/heizung.py > /scripts/heizung.py
+curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/scripts/requirements.txt > /scripts/requirements.txt
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.9
-cd /scripts/fenster/
+cd /scripts/
 pip install -r requirements.txt
