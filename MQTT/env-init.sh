f829ea4a27568a0db5ce3471752b413d3b961776
Asdoos <andi.riess@gmail.com>
2021-10-16 13:47:04 +0200

    wip

--- MQTT/env-init.sh
+++ MQTT/env-init.sh
@@ -1,4 +1,8 @@
 mkdir -p /etc/mosquitto/conf.d/
 curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/files/mosquitto.conf > /etc/mosquitto/conf.d/mosquitto.conf
+
+mkdir -p /scripts/fenster/
+curl -L https://raw.githubusercontent.com/Asdoos/katacoda-scenarios/main/MQTT/assets/scripts/fenster/main.py > /scripts/fenster/main.py
+
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.9
